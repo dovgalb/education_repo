995263ffb95345b439501759676d1a1d66a91748
dovgalb <bogdandovgal1208@gmail.com>
2023-12-21 17:19:46 +0300

    Protocol

--- trash/closures.py
+++ trash/closures.py
@@ -1,10 +1,42 @@
-def make_averager():
-    series = []
+class X:
+    def __init__(self,a):
+        self.a = a
 
-    def averager(new_value):
-        series.append(new_value)
-        total = sum(series)
-        return total / len(series)
+    def update_v(self, new):
+        self.a = new
+        print('Параметр был обновлен')
 
-    return averager
+    def print_data(self) -> None:
+        print(self.a, '- хуйня X')
 
+
+class Y:
+    def __init__(self,a):
+        self.a = a
+
+    def update_v(self, new):
+        self.a = new
+        print('Параметр был обновлен')
+
+    def print_data(self) -> None:
+        print(self.a, '- хуйня Y')
+
+
+def my_func(param: X, update_value: int) -> str:
+    param.update_v(update_value)
+    param.print_data()
+
+
+if __name__ == '__main__':
+    x = X(1)
+    y = Y(1)
+
+    my_func(y, 5)
+
+
+    def div(x: int, y: int):
+        return x / y
+
+
+    print(div(2, 3))
+    print(div("str1", "str2"))
