f452f6b3bb2a61f135efd26df14d477c8498f2c0
dovgalb <bogdandovgal1208@gmail.com>
2023-12-25 11:51:06 +0300

    some brockers

--- protocol_or_ABC/protocol/iot/diagnostic.py
+++ protocol_or_ABC/protocol/iot/diagnostic.py
@@ -2,11 +2,34 @@ from typing import Protocol
 
 
 class DiagnosticsSource(Protocol):
-    def update_status(self) -> str:
+    def update_status(self, status: str) -> bool:
         ...
 
 
-def diagnostic_device(source: DiagnosticsSource) -> None:
+class RedisSource:
+    def update_status(self, status: str) -> bool:
+        match status:
+            case "connected":
+                return True
+            case "disconnected":
+                return False
+
+
+class PostgresSource:
+
+    def __init__(self):
+        print('Реализовываем подключение к бд')
+        self.connection = dict()
+
+    def update_status(self, status: str) -> bool:
+        self.connection[status] = True
+        return self.connection[status]
+
+
+def diagnostic_device(source: DiagnosticsSource, status) -> None:
     print("Connecting to diagnostic server...")
-    status = source.update_status()
+    status = source.update_status(status)
     print(f"The status was update to [{status}]")
+
+
+diagnostic_device(PostgresSource(), "connected")
--- trash/closures.py
+++ trash/closures.py
@@ -1,42 +1,10 @@
-class X:
-    def __init__(self,a):
-        self.a = a
-
-    def update_v(self, new):
-        self.a = new
-        print('Параметр был обновлен')
-
-    def print_data(self) -> None:
-        print(self.a, '- хуйня X')
+import typing as t
 
 
-class Y:
-    def __init__(self,a):
-        self.a = a
-
-    def update_v(self, new):
-        self.a = new
-        print('Параметр был обновлен')
-
-    def print_data(self) -> None:
-        print(self.a, '- хуйня Y')
+# t.Iterable[int] - это протокол итераций
+def iterate_by(numbers: t.Iterable[int]) -> None:
+    for number in numbers:
+        print(number)
 
 
-def my_func(param: X, update_value: int) -> str:
-    param.update_v(update_value)
-    param.print_data()
-
-
-if __name__ == '__main__':
-    x = X(1)
-    y = Y(1)
-
-    my_func(y, 5)
-
-
-    def div(x: int, y: int):
-        return x / y
-
-
-    print(div(2, 3))
-    print(div("str1", "str2"))
+iterate_by(range(1_000))
